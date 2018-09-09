d1ecf56fb8c64bda5014417348d5ca425ba5dadb
Max Meinhold <mxmeinhold@gmail.com>
2018-09-09 03:29:04 -0400

    Adds mail
    There's no documentation, but I wrote it again....

---
+++ mail/Mail.java
@@ -0,0 +1,87 @@
+package mail;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Mail {
+
+    private static final String MAIL_HOST = null;
+    private static final String MAIL_USERNAME = null;
+    private static final String HOST = null;
+    
+    private static final String REQUEST_STRING = "%s is requesting a database for %s.\n\n"
+            + "<a href=%s/approve/%d>Approve</a>\n"
+            + "<a href=%s/deny/%d>Deny</a>\n\n-Deadass";
+    
+    private static final String APPROVAL_STRING = "Your database %s has been created. The password is <pre>%s</pre>\n\n-Deadass";
+    private static final String DENIAL_STRING = "Your database %s has been denied.\n\n-Deadass";
+
+    private MimeMessage msgTemplate;
+
+    Properties props;
+    Session session;
+    Transport transport;
+
+    private void sendMail(String subject, String body, String uid) {
+        try {
+            msgTemplate.setRecipient(Message.RecipientType.TO, new InternetAddress(uid + "@csh.rit.edu"));
+            msgTemplate.setSubject(subject);
+            msgTemplate.setContent(body, "text/html");
+            transport.connect(MAIL_HOST, MAIL_USERNAME, defaults.Secrets.MAIL_PASSWORD);
+
+            transport.sendMessage(msgTemplate, msgTemplate.getAllRecipients());
+
+            transport.close();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void request(String uid, String purpose, int dbID) {
+        String body = String.format(REQUEST_STRING, uid, purpose, HOST, dbID, HOST, dbID);
+        sendMail("Database Request", body, uid);
+    }
+    
+    public void approve(String uid, String dbName, String password) {
+        String body = String.format(APPROVAL_STRING, dbName, password);
+        sendMail("Database created", body, uid);
+    }
+    
+    public void deny(String uid, String dbName) {
+        String body = String.format(DENIAL_STRING, dbName);
+        sendMail("Database denial", body, uid);
+    }
+
+    public void init() {
+        props = System.getProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.port", 25);
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+
+        session = Session.getDefaultInstance(props);
+
+        try {
+            transport = session.getTransport();
+        } catch (NoSuchProviderException e1) {
+            System.err.println("Error: Mail found no provider.");
+        }
+
+        msgTemplate = new MimeMessage(session);
+        try {
+            msgTemplate.setFrom(new InternetAddress("deadass@csh.rit.edu", "Deadass"));
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
