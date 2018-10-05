9cdaa5d1b16e7ca94d2070e0d98197b386363e6a
Devin Matte <devinmatte@gmail.com>
2018-10-05 18:42:45 -0400

    Adding Baisc Springboot application

---
+++ src/api/Controller.java
@@ -0,0 +1,15 @@
+package api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String root() {
+        return "Hello World!";
+    }
+
+}
