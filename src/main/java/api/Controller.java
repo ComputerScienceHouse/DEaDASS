a7d7bf41a454a2abed1accf4f4d825f83fc7941c
Max Meinhold <mxmeinhold@gmail.com>
2018-12-17 16:11:03 -0500

    Makes routes return JSON

--- src/main/java/api/Controller.java
+++ src/main/java/api/Controller.java
@@ -16,39 +16,40 @@ import java.util.Map;
 @RestController
 public class Controller {
 
-    ManagerManager man; // TODO how is this being initialised?
+    ManagerManager man = new ManagerManager();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
         return "Hello World!";
     }
 
+    @RequestMapping(value="/message", method = RequestMethod.GET, produces = "application/json")
+    public String messageTest() {
+        return new Message("Testing message return", Message.Type.MESSAGE).asJSON();
+    }
+
 
     @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    public Message dbCreate(@RequestBody CreateRequest db) {
-        return man.request(db.getPoolID(), db.getName(), db.getPurpose(), db.getType());
+    public String dbCreate(@RequestBody CreateRequest db) {
+        return man.request(db.getPoolID(), db.getName(), db.getPurpose(), db.getType()).asJSON();
     }
 
 
     @RequestMapping(value = "/delete", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    public Message dbDelete(@RequestBody Map<String, String> body) {
-        return man.delete(body.get("database"));
+    public String dbDelete(@RequestBody Map<String, String> body) {
+        return man.delete(body.get("database")).asJSON();
     }
 
 
     @RequestMapping(value = "/approve", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    public Message dbApprove(@RequestBody Map<String, String> body) { // TODO this may be the wrong body annotation
-        return man.approve(body.get("database"));
+    public String dbApprove(@RequestBody Map<String, String> body) { // TODO this may be the wrong body annotation
+        return man.approve(body.get("database")).asJSON();
     }
 
 
     @RequestMapping(value = "/deny", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    public Message dbDeny(@RequestBody Map<String, String> body) {
-        return man.deny(body.get("database"));
+    public String dbDeny(@RequestBody Map<String, String> body) {
+        return man.deny(body.get("database")).asJSON();
     }
 
 }
--- src/main/java/api/model/Message.java
+++ src/main/java/api/model/Message.java
@@ -1,5 +1,11 @@
 package api.model;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Message {
     private String message;
     private Type type;
@@ -14,10 +20,18 @@ public class Message {
         Type(String type) {
             this.type = type;
         }
+
+        public String toString() {
+            return this.type;
+        }
     }
 
     public Message(String message, Type type) {
         this.message = message;
         this.type = type;
+    }
+
+    public String asJSON() {
+        return "{\"message\":\"" + this.message + "\", \"type\":\"" + this.type + "\"}";
     }
 }
