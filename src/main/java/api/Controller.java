ebc3e542ff138e1f68f2053720b0c6583abd58f2
Max Meinhold <mxmeinhold@gmail.com>
2019-01-18 19:37:05 -0500

    Removes JSONUtils from Controller

--- src/main/java/api/Controller.java
+++ src/main/java/api/Controller.java
@@ -2,7 +2,6 @@ package api;
 
 import api.model.Database;
 import api.model.DatabaseType;
-import api.model.JSONUtils;
 import api.model.exception.BadRequestException;
 import api.model.exception.NotFoundException;
 import api.model.exception.SQLException;
@@ -25,15 +24,15 @@ public class Controller {
     ManagerManager man = new ManagerManager();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String root() {
-        return "Hello World!";
+    public ResponseEntity<String> root() {
+        return ResponseEntity.status(200).body("Hello World!");
     }
 
 
     @RequestMapping(value="/databases", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> getDatabases() {
+    public ResponseEntity<Object> getDatabases() {
         try {
-            return ResponseEntity.status(200).body(JSONUtils.toJSON(man.listDatabases()));
+            return ResponseEntity.status(200).body(man.listDatabases());
         } catch (SQLException e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("SQL Exception");
@@ -42,7 +41,7 @@ public class Controller {
 
 
     @RequestMapping(value="/databases", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<String> createDatabase(@RequestBody Map<String, String> body) {
+    public ResponseEntity<Object> createDatabase(@RequestBody Map<String, String> body) {
         try {
             Database db = new Database(Integer.parseInt(body.get("pool_id")), body.get("db_name"),
                     body.get("purpose"), DatabaseType.fromString(body.get("type")));
@@ -59,9 +58,9 @@ public class Controller {
 
 
     @RequestMapping(value = "/databases/{database}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> getDatabase(@PathVariable(value = "database") String database) {
+    public ResponseEntity<Object> getDatabase(@PathVariable(value = "database") String database) {
         try {
-            return ResponseEntity.status(200).body(man.getDatabase(database).asJSON());
+            return ResponseEntity.status(200).body(man.getDatabase(database));
         } catch (SQLException e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("SQL Exception");
@@ -72,7 +71,7 @@ public class Controller {
 
 
     @RequestMapping(value="/databases/{database}", method = RequestMethod.DELETE, produces = "application/json")
-    public ResponseEntity<String> deleteDatabase(@PathVariable(value = "database") String database) {
+    public ResponseEntity<Object> deleteDatabase(@PathVariable(value = "database") String database) {
         try {
             return ResponseEntity.status(200).body(man.delete(database));
         } catch (SQLException e) {
@@ -85,7 +84,7 @@ public class Controller {
 
 
     @RequestMapping(value = "/databases/{database}/approval", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> checkPending(@PathVariable(value = "database") String database) {
+    public ResponseEntity<Object> checkPending(@PathVariable(value = "database") String database) {
         try {
             return ResponseEntity.status(200).body(man.isPending(database));
         } catch (SQLException e) {
@@ -97,10 +96,10 @@ public class Controller {
 
     // TODO: Consider merging into one PATCH route.
     @RequestMapping(value = "/databases/{database}/approval", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<String> approveDatabase(@PathVariable(value = "database") String database) {
+    public ResponseEntity<Object> approveDatabase(@PathVariable(value = "database") String database) {
         try {
-            String response = man.approve(database);
-            if(response.contains("password"))
+            Map<String, Object> response = man.approve(database);
+            if(response.containsKey("password"))
                 return ResponseEntity.status(201).body(response);
             else
                 return ResponseEntity.status(202).body(response);
@@ -116,7 +115,7 @@ public class Controller {
 
 
     @RequestMapping(value = "/databases/{database}/approval", method = RequestMethod.DELETE, produces = "application/json")
-    public ResponseEntity<String> denyDatabase(@PathVariable(value = "database") String database) {
+    public ResponseEntity<Object> denyDatabase(@PathVariable(value = "database") String database) {
         try {
             return ResponseEntity.status(200).body(man.deny(database));
         } catch (SQLException e) {
@@ -132,52 +131,52 @@ public class Controller {
 
     // Database User routes
     @RequestMapping(value = "/databases/{database}/users", method = RequestMethod.GET, produces = "appliction/json")
-    public ResponseEntity<String> getUsers(@PathVariable(value = "database") String database) {
+    public ResponseEntity<Object> getUsers(@PathVariable(value = "database") String database) {
         return ResponseEntity.status(501).body("Not yet implemented");
     }
 
 
     @RequestMapping(value = "/databases/{database}/users", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<String> createUser(@PathVariable(value = "database") String database, @RequestBody Map<String, String> body) {
+    public ResponseEntity<Object> createUser(@PathVariable(value = "database") String database, @RequestBody Map<String, String> body) {
         return ResponseEntity.status(501).body("Not yet implemented");
     }
 
 
     @RequestMapping(value = "/databases/{database}/users/{username}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> getUser(@PathVariable(value = "database") String database, @PathVariable(value = "username") String username) {
+    public ResponseEntity<Object> getUser(@PathVariable(value = "database") String database, @PathVariable(value = "username") String username) {
         return ResponseEntity.status(501).body("Not yet implemented");
     }
 
 
     @RequestMapping(value = "/databases/{database}/users/{username}", method = RequestMethod.DELETE, produces = "application/json")
-    public ResponseEntity<String> deleteUser(@PathVariable(value = "database") String database, @PathVariable(value = "username") String username) {
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "database") String database, @PathVariable(value = "username") String username) {
         return ResponseEntity.status(501).body("Not yet implemented");
     }
 
 
     @RequestMapping(value = "/databases/{database}/users/{username}/password", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<String> resetPassword(@PathVariable(value = "database") String database, @PathVariable(value = "username") String username) {
+    public ResponseEntity<Object> resetPassword(@PathVariable(value = "database") String database, @PathVariable(value = "username") String username) {
         Map<String, Object> message = new HashMap<String, Object>();
         message.put("status", "error");
         try {
             message.put("password", man.setPassword(database, username));
             message.put("status", "success");
-            return ResponseEntity.status(200).body(JSONUtils.toJSON(message));
+            return ResponseEntity.status(200).body(message);
         } catch (NotFoundException e) {
             message.put("message", e.getMessage());
-            return ResponseEntity.status(404).body(JSONUtils.toJSON(message));
+            return ResponseEntity.status(404).body(message);
         } catch (SQLException e) {
             e.printStackTrace();
             message.put("message", e.getMessage());
-            return ResponseEntity.status(500).body(JSONUtils.toJSON(message));
+            return ResponseEntity.status(500).body(message);
         }
     }
 
 
     @RequestMapping(value = "/pools", method = RequestMethod.GET, produces = "appliction/json")
-    public ResponseEntity<String> getPools() {
+    public ResponseEntity<Object> getPools() {
         try {
-            return ResponseEntity.status(200).body(JSONUtils.toJSON(man.listPools()));
+            return ResponseEntity.status(200).body(man.listPools());
         } catch (SQLException e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("SQL Exception");
@@ -186,15 +185,15 @@ public class Controller {
 
 
     @RequestMapping(value = "/pools", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<String> createPool(@RequestBody Map<String, String> body) {
+    public ResponseEntity<Object> createPool(@RequestBody Map<String, String> body) {
         return ResponseEntity.status(501).body("Not yet implemented");
     }
 
 
     @RequestMapping(value = "/pools/{id}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> getPool(@PathVariable(value = "id") int id) {
+    public ResponseEntity<Object> getPool(@PathVariable(value = "id") int id) {
         try {
-            return ResponseEntity.status(200).body(JSONUtils.toJSON(man.getPool(id)));
+            return ResponseEntity.status(200).body(man.getPool(id));
         } catch (SQLException e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("SQL Exception");
@@ -205,14 +204,14 @@ public class Controller {
 
 
     @RequestMapping(value = "/pools/{id}", method = RequestMethod.DELETE, produces = "application/json")
-    public ResponseEntity<String> deletePool(@PathVariable(value = "id") int id) {
+    public ResponseEntity<Object> deletePool(@PathVariable(value = "id") int id) {
         return ResponseEntity.status(501).body("Not yet implemented");
     }
 
 
     // Pool User Routes - can't get specific user by pool since they are only unique by database.
     @RequestMapping(value = "/pools/{id}/users", method = RequestMethod.GET, produces = "appliction/json")
-    public ResponseEntity<String> getUsers(@PathVariable(value = "id") int poolID) {
+    public ResponseEntity<Object> getUsers(@PathVariable(value = "id") int poolID) {
         return ResponseEntity.status(501).body("Not yet implemented");
     }
 }
