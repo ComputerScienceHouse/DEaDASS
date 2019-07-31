d6daf61a63365adf00cab2d3803368d3e235aa8f
Max Meinhold <mxmeinhold@gmail.com>
2019-07-30 23:25:57 -0400

    Disable warn on fail to connect to local mongo
    Spring attempts to connect to mongodb at localhost if it detects the
    driver in the classpath. This behaviour is resulting in warnings and
    exceptions at runtime, and I don't care for this behaviour at this time,
    so it can kindly be quiet.

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -1,10 +1,12 @@
 package api;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 import static org.springframework.boot.SpringApplication.run;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
