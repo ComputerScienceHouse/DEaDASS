f793a1d9e65aaac51f2a4ba126073c00c83e84ef
Max Meinhold <mxmeinhold@gmail.com>
2020-08-22 22:36:34 -0400

    Add ts, eslint, and webpack to the backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {},
+};
---
+++ backend/webpack.config.js
@@ -0,0 +1,32 @@
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  target: "node",
+  entry: "./src/main.ts",
+  plugins: [
+    new ESLintPlugin({
+      extensions: [".ts", ".tsx"],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+};
